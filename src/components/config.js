852563483207fd756aef3af539f3e33b29ce77d2
Gian Winckler <gian.cb@hotmail.com>
2019-09-01 10:46:02 -0300

    fix: barra de leitura inicia escondida

--- src/components/config.js
+++ src/components/config.js
@@ -2,21 +2,27 @@ exports.getStorage = function(){
     let obj;
 
     let appState = {
-        lang: "br"
+        lang: "en"
     }
 
-    try {
-        obj = JSON.parse(localStorage.getItem('gianw'));
-    }
-    catch (err) {
+    let objStorage = getObjStorage();
+
+    if (objStorage !== null) {
+        try {
+            obj = JSON.parse(objStorage.getItem('gianw'));
+        }
+        catch (err) {
+            obj = appState;
+            objStorage.setItem("gianw", JSON.stringify(obj));
+
+        }
+
+        if (!obj) {
+            obj = appState;
+            objStorage.setItem("gianw", JSON.stringify(obj));
+        }
+    }else{
         obj = appState;
-        localStorage.setItem("gianw", JSON.stringify(obj));
-
-    }
-
-    if (!obj) {
-        obj = appState;
-        localStorage.setItem("gianw", JSON.stringify(obj));
     }
 
     return obj;
@@ -24,5 +30,16 @@ exports.getStorage = function(){
 
 
 exports.setStorage = function(data){
-    localStorage.setItem("gianw", JSON.stringify(data));
+    let objStorage = getObjStorage();
+    if (objStorage !== null) {
+        objStorage.setItem("gianw", JSON.stringify(data));
+    }
+};
+
+function getObjStorage(){
+    if (typeof window !== 'undefined' && window.localStorage !== "undefined"){
+        return window.localStorage;
+    }else{
+        return null;
+    }
 };
